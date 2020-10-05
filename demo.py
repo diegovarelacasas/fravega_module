3a97be93c1501e31cd949b0a5f7fbb6f6044d164
diegovarelacasas <diegovarelacasas@gmail.com>
2020-10-05 00:09:36 -0300

    sql test create table and select

--- demo.py
+++ demo.py
@@ -3,9 +3,16 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="diego",
-    passwd="root"
+    passwd="root",
+    database="testdatabase"
     )
 
 mycursor = db.cursor()
 
-mycursor.execute("CREATE DATABASE testdatabase")
+#mycursor.execute("CREATE TABLE Person(name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+
+#mycursor.execute("INSERT INTO Person (name,age) VALUES (%s,%s)", ("joe", 22))
+#db.commit()
+mycursor.execute("SELECT * FROM Person")
+for x in mycursor:
+    print(x)
