a9e94a9bd9d84e0692946dcace365829b51a57e4
diegovarelacasas <diegovarelacasas@gmail.com>
2020-10-04 23:22:57 -0300

    sql test

--- demo.py
+++ demo.py
@@ -1,2 +1,11 @@
-print("Hello!")
-print("World")
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="diego",
+    passwd="root"
+    )
+
+mycursor = db.cursor()
+
+mycursor.execute("CREATE DATABASE testdatabase")
